6e50d6e7c5ad5baa31f1c8234fa731b0d8bdf62a
elena2020201 <elenarus1992@mail.ru>
2021-05-24 18:30:03 +0300

    Добавьте файлы проекта.

---
+++ godrojdpovozrasty/CodeFile1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+class DialogDemo
+{
+    static void Main()
+    {
+        string name;
+        name = Interaction.InputBox(
+            "Укажите Ваше имя", "Давайте познакомимся...");
+        string txt;
+        txt = ("Очень приятно," + name + " !");
+        MessageBox.Show(txt, "Знакомство состоялось");
+        string res;
+        int year = 2020, born, age;
+        res = Interaction.InputBox("Сколько Вам лет?", "Возраст");
+        age = Int32.Parse(res);
+        born = year - age;
+        res = "" + name + ", Вы родились в " + born + " году";
+        MessageBox.Show(res, "Год рождения");
+    }
+}
